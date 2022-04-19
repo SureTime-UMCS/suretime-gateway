76811505a640945a01666e070cdb30c2d924ef7a
Szymon Sala <ssala@9bits.pl>
2022-04-19 19:37:50 +0200

    init commit

---
+++ src/test/java/com/assigment/suretime/cloudgateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assigment.suretime.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
