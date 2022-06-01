54cfdea80dd3c30efc63ab9d3a150839f884457d
Szymon Sala <ssala@9bits.pl>
2022-06-01 13:20:39 +0200

    cookie to Authorization header

--- src/main/java/com/assigment/suretime/cloudgateway/CloudGatewayApplication.java
+++ src/main/java/com/assigment/suretime/cloudgateway/CloudGatewayApplication.java
@@ -2,11 +2,18 @@ package com.assigment.suretime.cloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class CloudGatewayApplication {
+
+	@Bean
+	public GlobalFilter customFilter() {
+		return new CustomGlobalFilter();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
